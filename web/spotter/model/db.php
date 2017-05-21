edb0d730b7685022f473f8509d2017ae4c088485
tallroids <tallroids@gmail.com>
2017-05-20 23:53:53 -0600

    updates for heroku db

--- web/spotter/model/db.php
+++ web/spotter/model/db.php
@@ -1,13 +1,28 @@
 <?php
 
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:password@localhost:5432/spotter";
+}
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+
 try
 {
-  $user = 'postgres';
-	$password = 'taLLr0ids*';
-	$db = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=spotter', $user, $password);
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $getPublicLocations = $db->query('SELECT title, description, coordinates FROM locations WHERE isPublic = true;');
-  $getFavoriteLocations = $db->query('SELECT title, description, coordinates, ispublic FROM locations;');
+  $getFavoriteLocations = $db->query('SELECT title, description, coordinates FROM locations;');
+  $getUserId = $db->query('SELECT title, description, coordinates, ispublic FROM locations;');
 }
 catch (PDOException $ex)
 {
